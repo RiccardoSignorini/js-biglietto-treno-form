1412f67f39c7854f288009b99dc0ed6e58b37d54
RiccardoSignorini <riccardo.signorini1@gmail.com>
2023-03-23 16:30:32 +0100

    avanzamento

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,27 +8,30 @@
 */
 
 // --VARIABILI--
-const lunghezzaPercorso = parseFloat(prompt(`Quanti km misura il percorso che vuoi fare?`));
-
-const age = parseFloat(prompt(`Quanti anni hai?`));
-
-// PREZZO NORMALE
-let price = (lunghezzaPercorso * (0.21));
-price = price.toFixed(2)
-
-// SCONTO UNDER 18
-let price1 = ((lunghezzaPercorso * (0.21))-(price/100*20));
-price1 = price1.toFixed(2)
-
-// SCONTO OVER 65
-let price2 = ((lunghezzaPercorso * (0.21))-(price/100*40));
-price2 = price2.toFixed(2)
-
-// --LOGICA--
-if (age<18) {
-    document.writeln(`<p> ${price1} €</p>`);
-} else if (age>65) {
-    document.writeln(`<p> ${price2} €</p>`);
-} else {
-    document.writeln(`<p> ${price} €</p>`);
+function makeTicket(){
+    // NOME E COGNOME
+    let firstLastName = document.querySelector(`#firstlastname`).value;
+    // ETA'
+    let age = document.querySelector(`#age`).value;
+    // LUNGHEZZA PERCORSO
+    let lunghezzaPercorso = document.querySelector(`#lunghezzapercorso`).value;
+    // PREZZO NORMALE
+    let price = (lunghezzaPercorso * (0.21));
+    price = price.toFixed(2);
+    // SCONTO UNDER 18
+    let price1 = ((lunghezzaPercorso * (0.21))-(price/100*20));
+    price1 = price1.toFixed(2);
+    // SCONTO OVER 65
+    let price2 = ((lunghezzaPercorso * (0.21))-(price/100*40));
+    price2 = price2.toFixed(2);
+    // --LOGICA--
+    document.querySelector(`#clientname`).innerHTML = `<p>${firstLastName}</p>`
+    
+    if (age<18) {
+        document.querySelector(`#price`).innerHTML = `<p>${price1}</p>`;
+    } else if (age>65) {
+        document.querySelector(`#price`).innerHTML = `<p>${price2}</p>`;
+    } else {
+        document.querySelector(`#price`).innerHTML = `<p>${price}</p>`;
+    }
 }
